605ef1fe8e144a6d28ca5880ce4a0c70b6bc1f75
gin <215129113@qq.com>
2022-10-17 22:27:20 +0800

    chore(init): initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="demo">123</div>
+</template>
+
+<style lang="scss" scoped>
+.demo {
+  @apply m-5 h-[200px] w-[200px] bg-violet-500 p-2;
+}
+</style>
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  arrowParens: 'avoid',
+  plugins: [require('prettier-plugin-tailwindcss')]
+}
